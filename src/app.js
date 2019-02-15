fa9d9ed96a07b202896c93dc0d4a2a68747bf478
mehdi-alouane <pro4medo@gmail.com>
2019-02-15 15:51:38 +0100

    add runDb() to app

--- src/app.js
+++ src/app.js
@@ -4,11 +4,14 @@ const logger = require('morgan')
 const app = express()
 
 const v1Router = require('./v1')
+const runDb = require('../db')
 
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/v1', v1Router)
+
+runDb()
 
 module.exports = app
