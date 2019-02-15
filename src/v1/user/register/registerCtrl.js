b2ea2c889ccdae79443100c7189f038251d0043b
mehdi-alouane <pro4medo@gmail.com>
2019-02-15 21:09:08 +0100

    add register controller

---
+++ src/v1/user/register/registerCtrl.js
@@ -0,0 +1,39 @@
+const { User } = require('../../../../db/models/userModel')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('../../../../config/config.json')
+
+module.exports = {
+  async registerUser (req, res) {
+    const { username, email, password } = req.body
+    const hashedPasswd = await bcrypt.hash(password, 10)
+    try {
+      const user = await User.create({
+        username: username,
+        email: email,
+        password: hashedPasswd
+      })
+
+      if (!user) {
+        return res.statsus(401).send({
+          msg: 'something wrong, please try again!'
+        })
+      }
+
+      const token = jwt.sign({
+        id: user.id
+      }, config.jwt.secret)
+
+      return res.status(200).send({
+        auth: true,
+        token: token
+      })
+    } catch (err) {
+      console.log(`Register Error: ${err.message}`)
+      return res.statsus(500).send({
+        auth: false,
+        msg: err.message
+      })
+    }
+  }
+}
