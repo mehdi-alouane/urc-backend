6ce2656a83951a037995a9c26657efe2abd56e55
mehdi-alouane <pro4medo@gmail.com>
2019-02-15 21:05:27 +0100

    add shops routes to v1Router

--- src/v1/index.js
+++ src/v1/index.js
@@ -1,7 +1,9 @@
 const v1Router = require('express').Router()
 
 const userRouter = require('./user')
+const shopsRouter = require('./shops')
 
 v1Router.use('/user', userRouter)
+v1Router.use('/shops', shopsRouter)
 
 module.exports = v1Router
