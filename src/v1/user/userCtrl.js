fdbb51d4149deb93a2aa873a3e4cd585c9489898
mehdi-alouane <pro4medo@gmail.com>
2019-02-15 21:04:23 +0100

    add user controller

---
+++ src/v1/user/userCtrl.js
@@ -0,0 +1,24 @@
+const { User } = require('../../../db/models/userModel')
+
+module.exports = {
+  async getUserDetails (req, res) {
+    try {
+      const { id } = req.user
+      const user = await User
+        .findOne({ id })
+        .select(['-_id', 'email', 'username'])
+
+      if (!user) {
+        return res.status(401).send({
+          msg: 'No user found.'
+        })
+      }
+      return res.status(200).send(user)
+    } catch (err) {
+      console.log(`UserCtrl error: ${err.message}`)
+      return res.status(500).send({
+        msg: err.message
+      })
+    }
+  }
+}
