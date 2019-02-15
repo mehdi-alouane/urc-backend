bbbc651385b90e935fe2daf450f5450f03a612cc
mehdi-alouane <pro4medo@gmail.com>
2019-02-15 21:08:05 +0100

    update userRouter routes

--- src/v1/user/index.js
+++ src/v1/user/index.js
@@ -1,3 +1,17 @@
 const userRouter = require('express').Router()
 
+const loginCtrl = require('./login/loginCtrl')
+const registerCtrl = require('./register/registerCtrl')
+const userCtrl = require('./userCtrl')
+const verifyToken = require('./verifyToken')
+
+userRouter.route('/login')
+  .post(loginCtrl.loginUser)
+
+userRouter.route('/register')
+  .post(registerCtrl.registerUser)
+
+userRouter.route('/me')
+  .get(verifyToken, userCtrl.getUserDetails)
+
 module.exports = userRouter
