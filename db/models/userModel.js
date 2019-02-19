97b75ea1e538fbf853ef0253bdce0c0f44b1c718
mehdi-alouane <pro4medo@gmail.com>
2019-02-19 10:19:09 +0100

    add preferredShops to userSchema

--- db/models/userModel.js
+++ db/models/userModel.js
@@ -5,6 +5,10 @@ const userSchema = mongoose.Schema({
   username: String,
   email: String,
   passwod: String,
+  preferredShops: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Shop'
+  }, { id: false }],
   created_At: {
     type: Date,
     default: Date.now
