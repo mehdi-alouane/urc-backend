456c826504375b10233222413d96925c51356c87
mehdi-alouane <pro4medo@gmail.com>
2019-02-19 10:35:06 +0100

    add user validation to user route

--- src/v1/user/index.js
+++ src/v1/user/index.js
@@ -1,15 +1,17 @@
 const userRouter = require('express').Router()
+const validate = require('express-validation')
 
 const loginCtrl = require('./login/loginCtrl')
 const registerCtrl = require('./register/registerCtrl')
 const userCtrl = require('./userCtrl')
 const verifyToken = require('./verifyToken')
+const validateUser = require('./validateUser')
 
 userRouter.route('/login')
-  .post(loginCtrl.loginUser)
+  .post(validate(validateUser.login), loginCtrl.loginUser)
 
 userRouter.route('/register')
-  .post(registerCtrl.registerUser)
+  .post(validate(validateUser.register), registerCtrl.registerUser)
 
 userRouter.route('/me')
   .get(verifyToken, userCtrl.getUserDetails)
