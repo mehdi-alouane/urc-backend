b13f3e966f118bf1dd60bf85a2ab6af174dd07d2
mehdi-alouane <pro4medo@gmail.com>
2019-02-21 15:21:14 +0100

    ref: update shopRouter routes

--- src/v1/shops/index.js
+++ src/v1/shops/index.js
@@ -1,8 +1,16 @@
 const shopsRouter = require('express').Router()
 
 const shopsCtrl = require('./shopsCtrl')
+const addTopreferredShops = require('./preferredShops/addToPreferredShops')
+const listPrefferedShops = require('./preferredShops/listPreferredShops')
 
-shopsRouter.route('/nearby')
-  .post(shopsCtrl.getNearByShops)
+shopsRouter.route('/nearby/:coordinates')
+  .get(shopsCtrl.getNearByShops)
+
+shopsRouter.route('/preferred-shops')
+  .post(addTopreferredShops)
+
+shopsRouter.route('/preferred-shops/:userId')
+  .get(listPrefferedShops)
 
 module.exports = shopsRouter
